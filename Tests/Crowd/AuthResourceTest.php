1108005d95d861f8abeb1e93fe5d67fa212eb46f
Paul Seiffert <paul.seiffert@gmail.com>
2013-03-22 22:19:25 +0100

    Added unit test environment and first test

---
+++ Tests/Crowd/AuthResourceTest.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Seiffert\CrowdRestBundle\Tests\Crowd;
+
+use Guzzle\Http\Client;
+use Guzzle\Http\Exception\BadResponseException;
+use Guzzle\Http\Exception\ClientErrorResponseException;
+use Guzzle\Http\Message\Request;
+use Guzzle\Http\Message\Response;
+use Seiffert\CrowdRestBundle\Crowd\AuthResource;
+
+/**
+ * @covers Seiffert\CrowdRestBundle\Crowd\AuthResource
+ */
+class AuthResourceTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var AuthResource
+     */
+    private $resource;
+
+    /**
+     * @var Client|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $httpClient;
+
+    public function setUp()
+    {
+        $this->resource = new AuthResource($this->getMockHttpClient());
+    }
+
+    public function testIsAuthenticationValidSuccess()
+    {
+        $username = 'test-user';
+        $password = 'test-passwort';
+
+        $this->getMockHttpClient()
+            ->expects($this->once())
+            ->method('post')
+            ->with('authentication?username=' . $username, null, json_encode(array('value' => $password)))
+            ->will($this->returnValue($this->createMockRequest()));
+
+        $this->assertTrue($this->resource->isAuthenticationValid($username, $password));
+    }
+
+    public function testIsAuthenticationValidInvalidCredentials()
+    {
+        $request = $this->createMockRequest(
+            function ($request) {
+                return $this->throwException(
+                    ClientErrorResponseException::factory($request, $this->createClientErrorResponse(400))
+                );
+            }
+        );
+
+        $this->getMockHttpClient()->expects($this->once())->method('post')->will($this->returnValue($request));
+
+        $this->assertFalse($this->resource->isAuthenticationValid('foo', 'bar'));
+    }
+
+    public function testIsAuthenticationValidServerError()
+    {
+        $request = $this->createMockRequest(
+            function ($request) {
+                return $this->throwException(
+                    BadResponseException::factory($request, $this->createMockResponse())
+                );
+            }
+        );
+
+        $this->getMockHttpClient()->expects($this->once())->method('post')->will($this->returnValue($request));
+
+        $this->setExpectedException('Guzzle\Http\Exception\BadResponseException');
+        $this->assertFalse($this->resource->isAuthenticationValid('foo', 'bar'));
+    }
+
+    /**
+     * @return Client|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getMockHttpClient()
+    {
+        if (null === $this->httpClient) {
+            $this->httpClient = $this->getMock('Guzzle\Http\Client');
+        }
+
+        return $this->httpClient;
+    }
+
+    /**
+     * @param callable $willAtSend
+     * @return Request|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function createMockRequest($willAtSend = null)
+    {
+        $request = $this->getMock('Guzzle\Http\Message\Request', array(), array(), '', false);
+
+        if (is_callable($willAtSend)) {
+            $request->expects($this->once())
+                ->method('send')
+                ->will($willAtSend($request));
+        }
+
+        return $request;
+    }
+
+    /**
+     * @return Response|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function createMockResponse()
+    {
+        return $this->getmock('Guzzle\Http\Message\Response', array(), array(), '', false);
+    }
+
+    /**
+     * @param int $status
+     * @return Response|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function createClientErrorResponse($status)
+    {
+        $response = $this->createMockResponse();
+
+        $response->expects($this->any())
+            ->method('isClientError')
+            ->will($this->returnValue(true));
+
+        $response->expects($this->any())
+            ->method('getStatusCode')
+            ->will($this->returnValue($status));
+
+        return $response;
+    }
+}
